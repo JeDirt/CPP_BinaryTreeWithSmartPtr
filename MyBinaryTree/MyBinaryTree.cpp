b0469a3713362f7b6bd3b3e6175caf1f0aa095ec
Martinyuk Denis <martynyuk.0.com@gmail.com>
2021-12-11 21:33:13 +0200

    refactored code

--- MyBinaryTree/MyBinaryTree.cpp
+++ MyBinaryTree/MyBinaryTree.cpp
@@ -3,9 +3,6 @@
 #include <memory>
 #include <algorithm>
 #include <stack>
-#include <ctime>
-#include <random>
-#include <chrono>
 
 
 using namespace std;
@@ -33,51 +30,49 @@ private:
 public:
 
 
-    BinaryTree( const T DataToPaste = T() ) : tree_size(0) { this->Root = nullptr; }
+    BinaryTree() : tree_size(0), Root(nullptr) { }
     virtual ~BinaryTree() {}
    
 
 public:
 
 
-    void push(const T DataToPaste)
+    void push(const T data)
     {
 
-        if (!Root) { Root = createNode(DataToPaste); }
-        else { push_helper(Root, DataToPaste); }
+        // If there is no Root, we create it
+        if (!Root) { Root = createNode(data); }
+
+        // If we have Root, then we go to find place for inserting new node
+        else { push_helper(Root, data); }
 
         tree_size++;
 
     }
-    T find(const T DataToFind)const
+    T find(const T to_find)const
     {
 
-        if (Root == nullptr) { return T(); }
+        // We haven't pushed any element yet
+        if (!Root) { throw exception("Tree is empty!"); }
 
-        if (Root.get()->Data == DataToFind) { return Root.get()->Data; }
-        else { find_helper(Root, DataToFind); }
+        // Coming to find
+        else { find_helper(Root, to_find); }
 
     }  
-    void delete_element(const T DataToDelete)
+    void delete_element(const T to_delete)
     {
 
-        Root = delete_helper(Root, DataToDelete);
+        // Handle new pointer(the case if we deleted Root, tree can be rebuild)
+        Root = delete_helper(Root, to_delete);
         tree_size--;
 
     }
 
 
-    T min()const
-    { 
-        auto min_value = min_helper(Root); 
-        return min_value.get()->Data; 
-    }
-    T max()const
-    {
-        auto max_value = max_helper(Root);
-        return max_value.get()->Data;
-    }
-  
+    // min/max helpers return pointer to node with least data
+    T min()const { return  min_helper(Root)->Data; }
+    T max()const { return max_helper(Root)->Data; }
+    
    
     void inorderTraversal() { inorderTraversal_helper(Root); }
     void preorderTraversal() { preorderTraversal_helper(Root); }
@@ -97,185 +92,193 @@ public:
 private:
 
 
-    shared_ptr<Node> createNode(const T DataToPaste)
+    shared_ptr<Node> createNode(const T data)
     {
 
         shared_ptr<Node> temp = make_shared<Node>(Node());
-        temp.get()->Data = DataToPaste;
+        temp->Data = data;
 
-        temp.get()->Left = nullptr;
-        temp.get()->Right = nullptr;
+        temp->Left = nullptr;
+        temp->Right = nullptr;
 
         return temp;
 
     }
 
 
-    void push_helper(const shared_ptr<Node> &Node, const T DataToPaste)
+    void push_helper(const shared_ptr<Node> &element, const T data)
     {
 
-        if (!Node) { return; }
+        if (!element) { return; }
 
-        if (Node.get()->Data > DataToPaste)
+        if (element->Data > data)
         {
-
-            if (!Node.get()->Left) { Node.get()->Left = createNode(DataToPaste); }
-            else { push_helper(Node.get()->Left, DataToPaste); }
-
+            // Coming left subtree and look for the place to insert
+            if (!element->Left) { element->Left = createNode(data); }
+            else { push_helper(element->Left, data); }
         }
         else 
         {
-            if (!Node.get()->Right) { Node.get()->Right = createNode(DataToPaste); }
-            else { push_helper(Node.get()->Right, DataToPaste); }
+            // Coming right subtree and look for the place to insert
+            if (!element->Right) { element->Right = createNode(data); }
+            else { push_helper(element->Right, data); }
         }
 
     }
-    T find_helper(const shared_ptr<Node>& Node, const T DataToFind)const
+    T find_helper(const shared_ptr<Node>& element, const T to_find)const
     {
 
-        if (!Node.get()) { return T(); }
-        if (Node.get()->Data == DataToFind) { return Node.get()->Data; }
+        // No such element
+        if (!element) { throw exception("No such element!"); }
 
-        if (DataToFind < Node.get()->Data) { return find_helper(Node.get()->Left, DataToFind); }
-        else { return find_helper(Node.get()->Right, DataToFind); }
+        // Check the case that searched element is Root
+        if (element->Data == to_find) { return element->Data; }
+
+        // Choose the subtree to continue looking for
+        if (to_find < element->Data) { return find_helper(element->Left, to_find); }
+        else { return find_helper(element->Right, to_find); }
 
     }
-    shared_ptr<Node> delete_helper(shared_ptr<Node> &node,T DataToDelete)
+    shared_ptr<Node> delete_helper(shared_ptr<Node> &element,T to_delete)
     {
 
-        if (!node) { return node; }
+        if (!element) { return element; }
 
-
-        if (DataToDelete < node.get()->Data)
+        // Looking for our element to delete
+        if (to_delete < element->Data)
         {
-            node.get()->Left = delete_helper(node.get()->Left, DataToDelete);
+            element->Left = delete_helper(element->Left, to_delete);
         }
 
-        else if (DataToDelete > node.get()->Data)
+        else if (to_delete > element->Data)
         {
-            node.get()->Right = delete_helper(node.get()->Right, DataToDelete);
+            element->Right = delete_helper(element->Right, to_delete);
         }
            
-
+        // We found element
         else 
         {
 
-            if (!node.get()->Left)
+            // If we have no left child
+            if (!element->Left)
             {
 
-                shared_ptr<Node> temp = node.get()->Right;
-                node.reset();
+                shared_ptr<Node> temp = element->Right;
+                element.reset();
 
                 return temp;
 
             }
 
-            else if (!node.get()->Right)
+            // If we have no right child
+            else if (!element->Right)
             {
 
-                shared_ptr<Node> temp = node.get()->Left;
-                node.reset();
+                shared_ptr<Node> temp = element->Left;
+                element.reset();
 
                 return temp;
 
             }
 
 
-            shared_ptr<Node> temp = min_helper(node.get()->Right);
+            shared_ptr<Node> temp = min_helper(element->Right);
 
-            node.get()->Data = temp.get()->Data;
-            node.get()->Right = delete_helper(node.get()->Right, temp.get()->Data);
+            element->Data = temp->Data;
+            element->Right = delete_helper(element->Right, temp->Data);
 
         }
 
-        return node;
+        return element;
 
     }
 
 
-    shared_ptr<Node> min_helper(const shared_ptr<Node>& Node)const
+    shared_ptr<Node> min_helper(const shared_ptr<Node>& element)const
     {
 
-        if (Node->Left != nullptr) { return min_helper(Node.get()->Left); }
-        else { return Node; }
+        // Dig left subtree until its done
+        if (element->Left) { return min_helper(element->Left); }
+        else { return element; }
 
     }
-    shared_ptr<Node> max_helper(const shared_ptr<Node>& Node)const
+    shared_ptr<Node> max_helper(const shared_ptr<Node>& element)const
     {
 
-        if (Node.get()->Right != nullptr) { return  max_helper(Node.get()->Right); }
-        else { return Node; }
+        // Dig right subtree until its done
+        if (element->Right) { return  max_helper(element->Right); }
+        else { return element; }
 
     }
 
 
-    void inorderTraversal_helper(const shared_ptr<Node>& Node)
+    void inorderTraversal_helper(const shared_ptr<Node>& element)
     {
 
-        if (!Node) { return; }
+        if (!element) { return; }
 
-        inorderTraversal_helper(Node.get()->Left);
-        cout << Node.get()->Data << " ";
-        inorderTraversal_helper(Node.get()->Right);
+        inorderTraversal_helper(element->Left);
+        cout << element->Data << " ";
+        inorderTraversal_helper(element->Right);
 
     }
-    void preorderTraversal_helper(const shared_ptr<Node> &Node)
+    void preorderTraversal_helper(const shared_ptr<Node> &element)
     {
 
-        if (!Node) { return; }
+        if (!element) { return; }
 
-        cout << Node.get()->Data << " ";
+        cout << element->Data << " ";
 
-        preorderTraversal_helper(Node.get()->Left);
-        preorderTraversal_helper(Node.get()->Right);
+        preorderTraversal_helper(element->Left);
+        preorderTraversal_helper(element->Right);
 
     }
-    void postorderTraversal_helper(const shared_ptr<Node> &Node)
+    void postorderTraversal_helper(const shared_ptr<Node> & element)
     {
 
-        if (!Node) { return; }
+        if (!element) { return; }
 
 
-        postorderTraversal_helper(Node.get()->Left);
-        postorderTraversal_helper(Node.get()->Right);
+        postorderTraversal_helper(element->Left);
+        postorderTraversal_helper(element->Right);
 
-        cout << Node.get()->Data << " ";
+        cout << element->Data << " ";
 
     }
 
 
-    void inorderTraversalByStack_helper(shared_ptr<Node> &node)
+    void inorderTraversalByStack_helper(shared_ptr<Node> &element)
     {
 
         stack<shared_ptr<Node>> _stack;
-        shared_ptr<Node> temp = node;
+        shared_ptr<Node> temp = element;
 
-        while (temp != nullptr || !_stack.empty())
+        while (temp || !_stack.empty())
         {
 
-            while (temp != nullptr)
+            while (temp)
             {
                 _stack.push(temp);
-                temp = temp.get()->Left;
+                temp = temp->Left;
             }
 
             temp = _stack.top();
             _stack.pop();
 
-            cout << temp.get()->Data << " ";
-            temp = temp.get()->Right;
+            cout << temp->Data << " ";
+            temp = temp->Right;
 
         }
 
     }
-    void preorderTraversalByStack_helper(shared_ptr<Node>& node)
+    void preorderTraversalByStack_helper(shared_ptr<Node>& element)
     {
 
-        if (!node) { return; }
+        if (!element) { return; }
 
 
         stack<shared_ptr<Node>> _stack;
-        _stack.push(node);
+        _stack.push(element);
 
 
         while (!_stack.empty())
@@ -284,23 +287,23 @@ private:
 
             shared_ptr<Node> temp = _stack.top();
 
-            cout << temp.get()->Data << " ";
+            cout << temp->Data << " ";
             _stack.pop();
 
 
-            if (temp.get()->Right) { _stack.push(temp.get()->Right); }
-            if (temp.get()->Left) { _stack.push(temp.get()->Left); }
+            if (temp->Right) { _stack.push(temp->Right); }
+            if (temp->Left) { _stack.push(temp->Left); }
 
 
         }
     }
-    void postorderTraversalByStack_helper(shared_ptr<Node>& node)
+    void postorderTraversalByStack_helper(shared_ptr<Node>& element)
     {
 
-        if (!node) { return; }
+        if (!element) { return; }
 
         stack <shared_ptr<Node>> _stack1, _stack2;
-        _stack1.push(node);
+        _stack1.push(element);
 
 
         while (!_stack1.empty())
@@ -311,8 +314,8 @@ private:
             _stack1.pop();
             _stack2.push(temp);
 
-            if (temp.get()->Left) { _stack1.push(temp.get()->Left); }
-            if (temp.get()->Right) { _stack1.push(temp.get()->Right); }
+            if (temp->Left) { _stack1.push(temp->Left); }
+            if (temp->Right) { _stack1.push(temp->Right); }
                 
         }
 
@@ -322,7 +325,7 @@ private:
 
             shared_ptr<Node> temp = _stack2.top();
 
-            cout << temp.get()->Data << " ";
+            cout << temp->Data << " ";
             _stack2.pop();
 
         }
@@ -330,29 +333,29 @@ private:
     }
 
 
-    void clear_helper(shared_ptr<Node> &node)
+    void clear_helper(shared_ptr<Node> & element)
     {
 
-        if (node.get()->Left) { clear_helper(node.get()->Left); }
-        if (node.get()->Right) { clear_helper(node.get()->Right); }
+        if (element->Left) { clear_helper(element->Left); }
+        if (element->Right) { clear_helper(element->Right); }
      
-        node.reset();
+        element.reset();
         tree_size--;
       
     }
 
 
     /*  Thanks to https ://github.com/RonGreenberg/print-binary-tree-to-console  */
-    void print_nodes_helper(const string& padding, const string& edge, shared_ptr<Node>node, bool has_left_sibling)
+    void print_nodes_helper(const string& padding, const string& edge, shared_ptr<Node>element, bool has_left_sibling)
     {
 
-        if (node != nullptr) 
+        if (element)
         { 
 
-            cout << endl << padding << edge << node.get()->Data;
+            cout << endl << padding << edge << element->Data;
 
           
-            if ((!node.get()->Left) && (!node.get()->Right))
+            if ((!element->Left) && (!element->Right))
             {
 
                 cout << endl << padding; 
@@ -365,23 +368,23 @@ private:
               
                 string new_padding = padding + (has_left_sibling ? "|    " : "     ");
 
-                print_nodes_helper(new_padding, "|----", node.get()->Right, node.get()->Left != nullptr);
-                print_nodes_helper(new_padding, "|____", node.get()->Left, false);
+                print_nodes_helper(new_padding, "|----", element->Right, element->Left != nullptr);
+                print_nodes_helper(new_padding, "|____", element->Left, false);
 
             }
 
         }
 
     } 
-    void print_tree_helper(shared_ptr<Node> &node)
+    void print_tree_helper(shared_ptr<Node> & element)
     {
         
-        if (!node) { return; }
+        if (!element) { return; }
          
-        cout << node.get()->Data;
+        cout << element->Data;
 
-        print_nodes_helper("", "|----", node.get()->Right, node.get()->Left != nullptr);
-        print_nodes_helper("", "|____", node.get()->Left, false);
+        print_nodes_helper("", "|----", element->Right, element->Left != nullptr);
+        print_nodes_helper("", "|____", element->Left, false);
 
     } 
 
@@ -396,92 +399,4 @@ private:
 };
 
 
-// Random generators for generating results
-auto CreateRandomGenerator(const int start_range, const int end_range)
-{
-
-    // An instance of an engine
-    random_device random_device;
-
-    // Specify the engine and distribution
-    mt19937 engine{ random_device() };
-    uniform_int_distribution<int> distribution{ start_range, end_range };
-
-
-    return  [&distribution, &engine]() { return distribution(engine); };
-
-}
-int GenerateRandomValue(const int start_range, const int end_range)
-{
-    return CreateRandomGenerator(start_range, end_range)();
-}
-vector<int> GenerateVector(const int start_range, const int end_range, int vector_length)
-{
-
-    if (vector_length < 0) { vector_length = 1; }
-
-    vector<int> temp(vector_length);
-  
-    for (size_t i = 0; i < vector_length; i++)
-    {
-        temp[i] = GenerateRandomValue(start_range, end_range);
-    }
-
-    return temp;
-
-}
-
-
-
-// Time trackers, used it to build some line charts
-
-/*
-auto begin = chrono::steady_clock::now();
-auto end = chrono::steady_clock::now();
-*/
-//auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
-// cout << "runtime = " << elapsed_ms.count() <<" ms/n"<< endl;
-
-
-
-// Demonstrating results of the work(needed only for personal purposes)
-int main()
-{
-
-   
-    BinaryTree<int>tree;
-    vector<int>test_data{ GenerateVector(1,100,10) };
-
-    cout << "Generated array -> ";
-    for_each(test_data.begin(), test_data.end(), [](auto v) {cout << v << " "; });
-    cout << endl << endl;
-
-
-
-    for (size_t i = 0; i < test_data.size(); i++)
-    {
-        tree.push(test_data[i]);
-    }
-
-   
-    tree.print_tree();
-    cout << endl;
-
-    cout << "Min element is " << tree.min() << endl;
-    cout << "Max element is " << tree.max() << endl;
-
-    int searched_and_deleted = GenerateRandomValue(1,100);
-    cout << endl << "Looking for element "<< searched_and_deleted <<" there we have " << tree.find(searched_and_deleted) << endl;
-
-    int searched = GenerateRandomValue(1, 100);
-    cout << "Looking for element " << searched << " there we have " << tree.find(searched) << endl;
-
-
-  
-    cout << "\n\nLet's delete element " << searched_and_deleted << ":" << endl << endl;
-    tree.delete_element(searched_and_deleted);
-    tree.print_tree();
-
-	return 0;
-
-}
+int main() { return 0; }
